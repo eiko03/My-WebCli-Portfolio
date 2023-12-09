37eada18d042dce7c34d54a10958acdcf581a3c9
Tipu Sultan EIko <tipusultan50@gmail.com>
2023-12-09 07:17:06 +0600

    - youtube iframe

--- src/components/WinboxComp.tsx
+++ src/components/WinboxComp.tsx
@@ -68,7 +68,6 @@ export const WinboxComponent = () => {
 
                      <iframe width="560" height="315"
                              src={winboxData}
-                             autoPlay={"1"}
                              title="YouTube video player" frameBorder="0"
                              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                              allowFullScreen></iframe>
